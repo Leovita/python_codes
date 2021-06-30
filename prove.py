a7c589f6016e6be3c046bc5f13e79b0c24528592
Leonardo Vitale <leonardo.vitale@fermi.mo.it>
2021-06-30 13:31:23 +0200

    add procode

--- prove.py
+++ prove.py
@@ -1,6 +1,13 @@
-dict = {"ciao": 2, "ciaone": 2}
+from math import log, ceil, floor, sqrt
+from itertools import count, islice
+import math
+# Allora se num < 0, skip, altrimenti se num == prime allora lui stesso, num non prime closest number power of 2
 
-for key in dict:
-    dict[key] = 0
+list_numbers = [9, 15, 33]
+# is_prime = lambda n: x > 1 and all(x % i for i in islice(count(2), int(sqrt(x)-1)))
+def next_power_of_2(x):  
+    return 1 if x == 0 else 2**(x - 1).bit_length()
 
-print(dict)
+solve = lambda l: [x if x > 1 and all(x % i for i in islice(count(2), int(sqrt(x)-1))) else 2**min(floor(log(x, 2)), ceil(log(x, 2)), key= lambda z: abs(x-2**z)) for x in l if x > 0]
+
+print(solve(list_numbers))
